bfab2ca3cf1ce66c10e9bf79e9a5bc0b6cf41051
Andrey <andreyavs.00@gmail.com>
2021-10-20 21:14:31 +0300

    add check string

--- script.js
+++ script.js
@@ -15,18 +15,21 @@ btn.addEventListener("click", () => {
 });
 
 function getWords(string) {
-  let arrWords = string.split(" ");
-  let words = {};
+  //Проверка на строку
+  if (typeof string === "string") {
+    let arrWords = string.split(" ");
+    let words = {};
 
-  for (let i = 0; i < arrWords.length; i++) {
-    if (words.hasOwnProperty(arrWords[i])) {
-      words[arrWords[i]] = words[arrWords[i]] + 1;
-    } else {
-      words[arrWords[i]] = 1;
+    for (let i = 0; i < arrWords.length; i++) {
+      if (words.hasOwnProperty(arrWords[i])) {
+        words[arrWords[i]] = words[arrWords[i]] + 1;
+      } else {
+        words[arrWords[i]] = 1;
+      }
     }
-  }
 
-  return words;
+    return words;
+  }
 }
 
 function sortWords(words) {
